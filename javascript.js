b11cf2bbbebaf104c8e824111d5444ac7cdebbd2
arbi hamolli <64432385+arbi222@users.noreply.github.com>
2024-06-01 21:41:59 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const mouseOver = () => {
+    const video = document.getElementById("video");
+    video.setAttribute("controls", "controls");
+}
+
+const mouseOut = () => {
+    const video = document.getElementById("video");
+    video.removeAttribute("controls");
+}
+
+const mouseOver2 = () => {
+    const video = document.getElementById("video2");
+    video.setAttribute("controls", "controls");
+}
+
+const mouseOut2 = () => {
+    const video = document.getElementById("video2");
+    video.removeAttribute("controls");
+}
+
+const mouseOver3 = () => {
+    const video = document.getElementById("video3");
+    video.setAttribute("controls", "controls");
+}
+
+const mouseOut3 = () => {
+    const video = document.getElementById("video3");
+    video.removeAttribute("controls");
+}
